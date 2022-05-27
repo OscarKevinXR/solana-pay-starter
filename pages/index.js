c8d3ec25ea13c2c82917222299278a39a7e5e995
Ousikai <mtfallsVR@protonmail.com>
2022-05-27 12:41:59 -0400

    🌐 Fetching items from the back room (IPFS)

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+
 import { PublicKey } from '@solana/web3.js';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
@@ -10,6 +12,18 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const App = () => {
   // This will fetch the users' public key (wallet address) from any wallet we support
   const { publicKey } = useWallet();
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      fetch(`/api/fetchProducts`)
+        .then(response => response.json())
+        .then(data => {
+          setProducts(data);
+          console.log("Products", data);
+        });
+    }
+  }, [publicKey]);
 
   const renderNotConnectedContainer = () => (
     <div>
@@ -18,6 +32,14 @@ const App = () => {
       <div className="button-container">
         <WalletMultiButton className="cta-button connect-wallet-button" />
       </div>    
+    </div>
+  );
+
+  const renderItemBuyContainer = () => (
+    <div className="products-container">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 
@@ -31,7 +53,7 @@ const App = () => {
 
         <main>
           {/* We only render the connect button if public key doesn't exist */}
-          {publicKey ? 'Connected!' : renderNotConnectedContainer()}
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
 
         </main>
 
